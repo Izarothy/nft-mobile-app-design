30937f22334a38eedd7bbedcd6be479eed345e61
Izarothy <izarothmail@gmail.com>
2022-02-06 23:10:49 +0100

    feat: display welcome based on localstorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head'
 import Welcome from '../components/Welcome'
-
+import { useState, useEffect } from 'react'
 export default function Home() {
-  return (
-    <div className="">
-      <Welcome />
-    </div>
-  )
+  const [userNew, setUserNew] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (!localStorage.userNew) {
+      localStorage.setItem('userNew', 'false')
+    } else {
+      setUserNew(false)
+    }
+  }, [])
+  return userNew ? <Welcome /> : <div>xd</div>
 }
