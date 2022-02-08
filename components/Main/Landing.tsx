2256ae7c142f65e02b66050ee79536badfd53239
Izarothy <izarothmail@gmail.com>
2022-02-08 21:06:04 +0100

    feat: main page based on state

---
+++ components/Main/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <main className='h-full bg-[url("/images/Banner.png")]'>
+      <Header />
+    </main>
+  )
+}
+
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,12 @@
 import Welcome from '../components/Welcome'
+import Landing from '../components/Main/Landing'
 import { useState, useEffect } from 'react'
 export default function Home() {
   const [userNew, setUserNew] = useState<boolean>(true)
-
   useEffect(() => {
-    if (!localStorage.userNew) {
-      localStorage.setItem('userNew', 'false')
-    } else {
-      setUserNew(false)
-    }
+    localStorage.userNew
+      ? setUserNew(false)
+      : localStorage.setItem('userNew', 'true')
   }, [])
-  return userNew ? <Welcome /> : <div>xd</div>
+  return userNew ? <Welcome /> : <Landing />
 }
