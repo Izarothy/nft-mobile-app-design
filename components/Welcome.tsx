935606daf141ac4b7a83f4049a94e8ba02a17432
Izarothy <izarothmail@gmail.com>
2022-02-09 21:47:52 +0100

    fix: hide welcome screen tablet+ as well

--- components/Welcome.tsx
+++ components/Welcome.tsx
@@ -6,37 +6,42 @@ interface Props {
 }
 const Welcome = ({ setUserNew }: Props) => {
   return (
-    <div className="relative h-screen overflow-x-hidden bg-primary-dark pt-8">
-      <h1 className="mx-4 text-[180%] font-bold text-white">
-        Discover, collect and sell extraordinary NFTs
-      </h1>
-      <span className="absolute top-36 -right-4 z-10 mb-8 w-4/5">
-        <Image
-          src="/images/welcome-1.png"
-          width={387}
-          height={628}
-          objectFit="cover"
-          quality="100"
-        />
-      </span>
-      <span className="absolute -bottom-28 w-4/5 ">
-        <Image
-          src="/images/welcome-2.png"
-          width={370}
-          height={280}
-          objectFit="cover"
-          quality="100"
-        />
-      </span>
-      <div className="flex w-full justify-center">
-        <button
-          className="px-auto fixed bottom-2 z-20 mx-auto w-5/6 rounded-lg bg-primary-violet py-3 text-sm"
-          onClick={() => setUserNew(false)}
-        >
-          Let's Get Started
-        </button>
+    <main className="h-screen overflow-x-hidden bg-primary-dark pt-8">
+      <div className="relative md:hidden">
+        <h1 className="mx-4 text-[180%] font-bold text-white">
+          Discover, collect and sell extraordinary NFTs
+        </h1>
+        <span className="absolute top-36 -right-4 z-10 mb-8 w-4/5">
+          <Image
+            src="/images/welcome-1.png"
+            width={387}
+            height={628}
+            objectFit="cover"
+            quality="100"
+          />
+        </span>
+        <span className="absolute -bottom-28 w-4/5 ">
+          <Image
+            src="/images/welcome-2.png"
+            width={370}
+            height={280}
+            objectFit="cover"
+            quality="100"
+          />
+        </span>
+        <div className="flex w-full justify-center">
+          <button
+            className="px-auto fixed bottom-2 z-20 mx-auto w-5/6 rounded-lg bg-primary-violet py-3 text-sm"
+            onClick={() => setUserNew(false)}
+          >
+            Let's Get Started
+          </button>
+        </div>
+        <div className="hidden h-screen md:block">
+          This layout is only available for mobile
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
 
